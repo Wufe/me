4a059a73f97c37bfcdc0bb9ef9e354494ccfd07f
Wufe <wufe92@gmail.com>
2016-09-23 14:03:36 +0200

    Updated webhook

--- src/webhook/index.js
+++ src/webhook/index.js
@@ -3,6 +3,7 @@ import BabelPolyfill from 'babel-polyfill';
 import express from 'express';
 import BodyParser from 'body-parser';
 import xhub from 'express-x-hub';
+import {exec, spawn} from 'child_process';
 
 let app = express();
 let port = 1341;
@@ -21,13 +22,24 @@ app.use( xhub({
 	app.post( `/webhook`, (req, res) => {
 		console.log( "Post request body:" );
 		console.log( req.body );
-		if( req.isXHub && req.isXHubValid() ){
-			res.send( "Received." );
-			console.log( "Acceptable." );
-		}else{
-			res.send( "Rejected." );
-			console.log( "Wrong secret." );
-		}
+		console.log( "Got it." );
+		let args = [
+            "pull"
+        ];
+        let cmd = spawn( "git", args );
+        cmd.stdout.on( 'data', ( data ) => {
+
+        });
+        cmd.stderr.on( 'data', ( data ) => {
+
+        });
+        cmd.on( 'error', ( err ) => {
+            console.log( err );
+        });
+        cmd.on( 'close', ( code ) => {
+            console.log( "Done." );
+        });
+		req.send( "OK" );
 	}).get( `/webhook`, (req, res) => {
 		console.log( "Received a get request." );
 		res.send( "Try with a post request" );
