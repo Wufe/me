0618c44c480594a8f216f9009eecdc2c20ae6f94
Wufe <wufe92@gmail.com>
2016-09-23 13:48:12 +0200

    webhook service wip

--- src/webhook/index.js
+++ src/webhook/index.js
@@ -2,6 +2,7 @@ import BabelRegister from 'babel-register';
 import BabelPolyfill from 'babel-polyfill';
 import express from 'express';
 import BodyParser from 'body-parser';
+import xhub from 'express-x-hub';
 
 let app = express();
 let port = 1341;
@@ -11,15 +12,22 @@ app.use( BodyParser.urlencoded({
 app.use( BodyParser.json() );
 
 let secret = process.env.WEBHOOK_SECRET;
+app.use( xhub({
+	algorithm: 'sha1',
+	secret
+}));
 
 (() => {
 	app.post( `/webhook`, (req, res) => {
 		console.log( "Post request body:" );
 		console.log( req.body );
-		console.log( "\n\n\n\n\n\n\n\n\n\nSecret:" );
-		console.log( req.headers[ 'x-hub-signature' ] );
-		//test
-		res.send( "Received." );
+		if( req.isXHub && req.isXHubValid() ){
+			res.send( "Received." );
+			console.log( "Acceptable." );
+		}else{
+			res.send( "Rejected." );
+			console.log( "Wrong secret." );
+		}
 	}).get( `/webhook`, (req, res) => {
 		console.log( "Received a get request." );
 		res.send( "Try with a post request" );
