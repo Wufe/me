a07527bc3bdfe9fea2e3804f2eb8130367186acd
Wufe <wufe92@gmail.com>
2016-09-27 11:51:54 +0200

    No need to remove tty from watch routine

--- environment/make/Watch.mk
+++ environment/make/Watch.mk
@@ -1,3 +1,3 @@
 watch:
 	${INFO} "Starting now webpack watch routine.."
-	${CMD} $(DOCKER_RUN) --rm -w /app -v `pwd`:/app node:wheezy npm run pack:watch -s
+	${CMD} $(DOCKER_RUN) -it --rm -w /app -v `pwd`:/app node:wheezy npm run pack:watch -s
